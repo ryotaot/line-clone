01001432ec8951950268a187161dbd9b949c8ed9
ryota <ryota200309@email.com>
2025-02-26 21:37:33 +0900

    firebaseから表示

---
+++ src/components/SendMessage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SendMessage = () => {
+  return (
+    <div>
+      <form></form>
+    </div>
+  );
+};
+
+export default SendMessage;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button } from "@mui/material";
+import firebase from "firebase/compat/app";
+import { auth } from "../firebase";
 
 const SignIn = () => {
   const SignInWithGoogle = () => {
     // Google認証を行う処理
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
   };
 
   return (
