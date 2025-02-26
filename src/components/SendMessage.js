e1e8eabd142a5f1742497c42b1c5e6f8cb9e2f66
ryota <ryota200309@email.com>
2025-02-26 21:53:02 +0900

    メッセージ機能

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import { db, auth } from "../firebase.js";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 const SendMessage = () => {
   const [message, setMessage] = useState("");
+  function sendMessage(e) {
+    e.preventDefault();
+
+    const { photoURL, uid } = auth.currentUser;
+
+    db.collection("messages").add({
+      text: message,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setMessage("");
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={sendMessage}>
         <div className="sendMsg">
-          <input placeholder="メッセージを入力" type="text" />
+          <input
+            placeholder="メッセージを入力"
+            type="text"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
         </div>
       </form>
     </div>
